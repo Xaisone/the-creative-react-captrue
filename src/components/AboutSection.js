4fd1c59529ceb26f568d935c862ba68e0555cd33
Xaisone <143376940+Xaisone@users.noreply.github.com>
2025-04-23 21:18:50 +0700

    5.Intro To Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,71 @@
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const AboutSection = () => {
+    
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StyleHide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyleHide>
+          <StyleHide>
             <h2>
               Your <span> dreams</span>
             </h2>
-          </div>
-          <div className="hide">
+          </StyleHide>
+          <StyleHide>
             <h2>comse true.</h2>
-          </div>
+          </StyleHide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage>
         <img src={home1} alt="" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+
+
+// Style Here
+
+const StyledAbout = styled.div`
+ min-height: 90vh;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ padding: 5rem 10rem;
+ color: white;
+`
+const StyledDescription = styled.div`
+flex: 1;
+padding-right: 5rem;
+h2{
+    font-weight: lighter;
+}
+`
+
+const StyledImage =  styled.div`
+
+flex: 1;
+overflow: hidden;
+
+img{
+    height: 80vh;
+    width: 100%;
+    object-fit: cover;
+}
+`
+
+const StyleHide = styled.div`
+overflow: hidden;
+`
 
 export default AboutSection;
