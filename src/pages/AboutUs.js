03e02a598bb01d55942eebfda74fbac3c2d05f26
Xaisone <143376940+Xaisone@users.noreply.github.com>
2025-04-23 20:09:47 +0700

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
-const AboutUs = () =>{
-
-    return(
-        <AboutSection />
-        
-    )
-}
+const AboutUs = () => {
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
+};
 
 export default AboutUs;
